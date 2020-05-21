ada35da13e854f956f09ee8a0899cac645ad515e
Alexander Törnvall <alextorn.business@gmail.com>
2020-05-21 11:51:51 +0200

    Updates

---
+++ precache-manifest.6c4668a8efe607cab5f56fe10962d602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d35bcb028ae39917d28aa3ed456cd4",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "668d8d1f53050b7c80e3",
+    "url": "/react-monsters-rolodex/static/css/main.34a54f89.chunk.css"
+  },
+  {
+    "revision": "708d6393b709640cb167",
+    "url": "/react-monsters-rolodex/static/js/2.b3ff46a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.b3ff46a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668d8d1f53050b7c80e3",
+    "url": "/react-monsters-rolodex/static/js/main.cf9ddb9c.chunk.js"
+  },
+  {
+    "revision": "84d462a1ad20fdb403f4",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.6eda5f3f.js"
+  }
+]);
